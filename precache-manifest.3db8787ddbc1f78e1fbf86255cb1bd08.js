ad78f55c2266c1c4a04fcd8d858953ec9c9f95a4
katsmunoz <katherine.smm@gmail.com>
2020-07-16 17:22:10 -0400

    Updates

---
+++ precache-manifest.3db8787ddbc1f78e1fbf86255cb1bd08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fa31a73976953f259368284d7fd862",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "8be460468728f3558b1e",
+    "url": "/todo-list/static/css/main.99b18270.chunk.css"
+  },
+  {
+    "revision": "6977dcb4321efbb4c9ca",
+    "url": "/todo-list/static/js/2.bad6ca91.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.bad6ca91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be460468728f3558b1e",
+    "url": "/todo-list/static/js/main.79365e30.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
