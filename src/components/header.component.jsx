743abba5a168cc6b907db8778e183cf1692feed2
katsmunoz <katherine.smm@gmail.com>
2020-07-16 20:09:31 -0400

    first commit

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -3,7 +3,7 @@ import {AppBar, Toolbar } from '@material-ui/core';
 import logo from './../assets/img/checklist.png';
 
 const Header = () => (
-    <AppBar position="sticky" style={{ marginBottom: 5 }} >
+    <AppBar position="sticky" style={{ marginBottom: 5, justifyContent: 'center' }} >
         <Toolbar>
             <img src={logo} alt="logo" width="32" height="32"></img>
             &nbsp;&nbsp;
