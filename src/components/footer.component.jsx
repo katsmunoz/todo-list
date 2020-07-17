15c21f3583348334839ab73e7f5c45d1405ebb5b
katsmunoz <katherine.smm@gmail.com>
2020-07-17 17:10:11 -0400

    fixing footer

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './footer.style.css';
+import './footer.styles.css';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import { Typography } from '@material-ui/core';
 
 const Footer = () => (
-    <footer>
+    <footer className="footer">
         <Typography style={{ marginTop: '10px', fontWeight: 400 }} >&copy; { new Date().getFullYear() } - Made with<FavoriteIcon style={{ color: 'red', height: '16px' }} />
-        by <a href='https://katsmunoz.github.io/' target="_blank">Katherine Muñoz</a>
+        by <a href='https://katsmunoz.github.io/' target="_blank">Katsmunoz</a>
             </Typography>
     </footer>
 );
